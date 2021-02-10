cc7c5f7839bd1b9b592eafa3a46f6c10e3b32ecc
min0ru <2010min0ru@gmail.com>
2021-02-10 18:49:45 +0300

    Improved episode testing.

--- tests/test_episode.py
+++ tests/test_episode.py
@@ -15,10 +15,16 @@ class AbstractAgentCase(abc.ABC):
 
     def test_environment_output(self):
         """ Play one episode with RandomAgent and test that env output is not empty. """
-        observations, actions, rewards = self.env.play_episode(self.agent, render=False)
-        self.assertTrue(len(observations) > 0)
-        self.assertTrue(len(actions) > 0)
-        self.assertTrue(len(rewards) > 0)
+        observations, actions, rewards, total_reward = self.env.play_episode(self.agent)
+        observations_num = len(observations)
+        actions_num = len(actions)
+        rewards_num = len(rewards)
+
+        self.assertEqual(observations_num, actions_num)
+        self.assertEqual(observations_num, rewards_num)
+        self.assertGreater(observations_num, 0)
+
+        self.assertNotEqual(total_reward, 0)
 
 
 class RandomAgentCase(AbstractAgentCase, unittest.TestCase):
