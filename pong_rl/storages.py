ec9b65a19c0951e5d8cfd9de51e73964f1d222f2
min0ru <2010min0ru@gmail.com>
2021-02-22 00:38:06 +0300

    MultiList class implementation.

--- pong_rl/storages.py
+++ pong_rl/storages.py
@@ -1,5 +1,33 @@
 
 
+class MultiList:
+    """ MultiList is a list of lists of the same length. """
+    def __init__(self, length):
+        if length <= 0:
+            raise TypeError('Number of sublists should be more than one!')
+        self.length = length
+        self.list = [list() for _ in range(length)]
+
+    def append(self, iterable):
+        """ Append new elements from a given iterable by spreading it over sublists. """
+        if len(iterable) != self.length:
+            raise TypeError('Appended sublist should be the same length as MultiList!')
+        for stored_sublist, new_element in zip(self.list, iterable):
+            stored_sublist.append(new_element)
+
+    def flat(self):
+        """ Return flat list by uniting sublists. """
+        return sum(self.list, [])
+
+    def __iter__(self):
+        """ MultiList could be iterated as list of sublists. """
+        return iter(self.list)
+
+    def __len__(self):
+        """ MultiList length is a number of sublists. """
+        return len(self.list)
+
+
 class EpisodeStorage:
     """ Storage for output data of a multiple environments.
 
