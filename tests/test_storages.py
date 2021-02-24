b9c3faf6a94e3d3f9617e4a8d22835e0783d3568
min0ru <2010min0ru@gmail.com>
2021-02-24 20:56:56 +0300

    Fixed error in EpisodeStorage test.

--- tests/test_storages.py
+++ tests/test_storages.py
@@ -79,7 +79,7 @@ class EpisodeStorageCase(unittest.TestCase):
         # Check that environment data is stacked in sequential way and right order.
         self.assertListEqual(observations, [1, 1, 1, 2, 2, 2, 3, 3, 3])
         self.assertListEqual(actions, [5, 5, 5, 6, 6, 6, 7, 7, 7])
-        self.assertListEqual(rewards, [5, 5, 5, 6, 6, 6, 7, 7, 7])
+        self.assertListEqual(rewards, [8, 8, 8, 9, 9, 9, 10, 10, 10])
         self.assertListEqual(infos, [11, 11, 11, 12, 12, 12, 13, 13, 13])
 
 
