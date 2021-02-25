b53374529fe87993de4803c1f4a54f18b12cc073
min0ru <2010min0ru@gmail.com>
2021-02-25 15:42:38 +0300

    Redesigned project demo / training routine.

--- pong_rl/__main__.py
+++ pong_rl/__main__.py
@@ -1,62 +1,54 @@
-from pong_rl.agents import PongAgentTF
-from pong_rl.environments import PongEnvironment
-from pong_rl.timer import ContextTimer
-
 import numpy as np
+from pathlib import Path
 
-
-def size_mb(nparray):
-    return round(nparray.nbytes / 1024 / 1024, 2)
+from pong_rl.agents import PongAgentTF, PongAgentRandom
+from pong_rl.environments import PongEnvironment, VectorizedPongEnvironment
+from pong_rl.timer import ContextTimer
 
 
 def main():
-    pong = PongEnvironment()
-    # agent_rnd = PongAgentRandom()
+    pong = VectorizedPongEnvironment(num_environments=32)
+    pong_render = PongEnvironment()
     agent_tf = PongAgentTF()
 
+    saved_model = 'data/model.dat'
+    if Path(saved_model).exists():
+        print('Loading saved model weights')
+        agent_tf._model.load_weights(saved_model)
+
     print(agent_tf.summary)
-    with ContextTimer('Total Time (PongAgentTF) WARMUP!'):
-        observations, actions, rewards = np.array([]), np.array([]), np.array([])
-        warmup_episode_num = 64
-        for episode in range(warmup_episode_num):
-            with ContextTimer(f'Episode {episode}'):
-                ep_observations, ep_actions, ep_rewards, ep_score = \
-                    pong.play_episode(agent_tf, render=False)
 
-                print('Episode observations number:', len(ep_observations))
-                print('Episode score:', ep_score)
-
-                if observations.any():
-                    observations = np.append(observations, ep_observations, axis=0)
-                    actions = np.append(actions, ep_actions, axis=0)
-                    rewards = np.append(rewards, ep_rewards, axis=0)
-                else:
-                    observations = ep_observations
-                    actions = ep_actions
-                    rewards = ep_rewards
-
-        print('')
-        print(f'Total observations length {len(observations)} after {episode} episodes')
-        print(f'Observation shape: {observations.shape} size: {size_mb(observations)} MB')
-        print(f'Actions shape: {actions.shape} size: {size_mb(actions)} MB')
-        print(f'Rewards shape: {rewards.shape} size: {size_mb(rewards)} MB')
-
-    print('Warmup finished! Training!')
-    agent_tf.train(observations, actions, rewards, verbose=True)
-
+    episode = 0
     should_train = True
     while should_train:
-        ep_observations, ep_actions, ep_rewards, ep_score = \
-            pong.play_episode(agent_tf, render=True)
+        print(f"Starting [{episode}] episode")
+        with ContextTimer("Episode Timer"):
+            ep_observations, ep_actions, ep_rewards, ep_score = \
+                pong.play_episode(agent_tf, render=False)
 
-        print('Episode observations number:', len(ep_observations))
-        print(f'Episode [{episode}] score:', ep_score)
+        print(f'Episode [{episode}] observations number: {len(ep_observations)}')
+        print(f'Episode [{episode}] score: {ep_score}')
 
         print('Actions:\n', ep_actions)
-        # print('Rewards:\n', ep_rewards)
+        print('Rewards:\n', ep_rewards)
 
-        agent_tf.train(ep_observations, ep_actions, ep_rewards)
+        with ContextTimer("Training Timer"):
+            agent_tf.train(
+                ep_observations,
+                ep_actions,
+                ep_rewards,
+                batch_size=2048,
+            )
         episode += 1
+
+        print('Saving model weights')
+        agent_tf._model.save_weights('data/model.dat')
+
+        # if episode % 5 == 0:
+        print('Playing renderable demo episode')
+        for _ in range(4):
+            pong_render.play_episode(agent_tf, render=True)
+        print('Finished demo episode')
 
 
 if __name__ == '__main__':
