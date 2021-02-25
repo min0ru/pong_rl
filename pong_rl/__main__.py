886be78e6629cdaacc93efe74f78a039e74a531b
min0ru <2010min0ru@gmail.com>
2021-02-26 01:29:15 +0300

    New training routine with multiprocess rendering.

--- pong_rl/__main__.py
+++ pong_rl/__main__.py
@@ -1,21 +1,55 @@
-import numpy as np
+from multiprocessing import Process, Pipe
 from pathlib import Path
+import pickle
 
 from pong_rl.agents import PongAgentTF, PongAgentRandom
 from pong_rl.environments import PongEnvironment, VectorizedPongEnvironment
 from pong_rl.timer import ContextTimer
 
 
-def main():
-    pong = VectorizedPongEnvironment(num_environments=32)
-    pong_render = PongEnvironment()
-    agent_tf = PongAgentTF()
+SAVED_MODEL = 'data/model.dat'
 
-    saved_model = 'data/model.dat'
+
+def renderer(pipe, environment, saved_model):
+    """ Receive actual agent from pipe and render it in provided environment. """
+    print('[render_process]: Render process started')
+    agent = PongAgentTF()
+
+    print('[render_process]: Awaiting for new agent data')
+    episode = pipe.recv()
+    print('[render_process]: Received agent update signal')
+
+    if Path(saved_model).exists():
+        agent._model.load_weights(saved_model)
+        print('[render_process]: Loaded Weights')
+
+    print('[render_process]: Starting episodes rendering')
+
+    should_run = True
+    while should_run:
+        environment.play_episode(agent, render=True)
+        if pipe.poll():
+            episode = pipe.recv()
+            if Path(saved_model).exists():
+                agent._model.load_weights(saved_model)
+            print(f'[render_process] Received and updated new agent from episode {episode}')
+
+
+def main():
+    pong = VectorizedPongEnvironment(num_environments=128)
+    pong_render = PongEnvironment()
+
+    saved_model = SAVED_MODEL
     if Path(saved_model).exists():
         print('Loading saved model weights')
         agent_tf._model.load_weights(saved_model)
 
+    print('Starting rendering process')
+    child_pipe, parent_pipe = Pipe()
+    render_process = Process(target=renderer, args=(child_pipe, pong_render, saved_model))
+    render_process.start()
+
+    agent_tf = PongAgentTF()
     print(agent_tf.summary)
 
     episode = 0
@@ -44,11 +78,12 @@ def main():
         print('Saving model weights')
         agent_tf._model.save_weights('data/model.dat')
 
-        # if episode % 5 == 0:
-        print('Playing renderable demo episode')
-        for _ in range(4):
-            pong_render.play_episode(agent_tf, render=True)
-        print('Finished demo episode')
+        # Updating rendering agent
+        print('Updating rendering agent')
+        parent_pipe.send(episode)
+
+
+    render_process.join()
 
 
 if __name__ == '__main__':
