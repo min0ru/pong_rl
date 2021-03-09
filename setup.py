86f6f5c1a367aff300ddef3f3cce56c05a55db10
min0ru <2010min0ru@gmail.com>
2021-03-09 20:11:33 +0300

    New tools for project linting, formatting and testing.

--- setup.py
+++ setup.py
@@ -3,39 +3,37 @@ https://packaging.python.org/guides/distributing-packages-using-setuptools/
 https://github.com/pypa/sampleproject
 """
 
-from setuptools import setup, find_packages
 import pathlib
+
+from setuptools import find_packages, setup
 
 here = pathlib.Path(__file__).parent.resolve()
 
 # Get the long description from the README file
-long_description = (here / 'README.md').read_text(encoding='utf-8')
+long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
-    name='pong_rl',
-    version='0.0.1',
-    description='A sample Python project',
+    name="pong_rl",
+    version="0.0.1",
+    description="A sample Python project",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/min0ru/pong_rl',
-    author='min0ru',
-    author_email='2010min0ru@gmail.com',
-    packages=find_packages(where='pong_rl'),
-    python_requires='>=3.6, <4',
-
-    install_requires=[
-        'gym[atari]',
-        'tensorflow==2.4.1',
-        'numpy==1.19.2',
-        'scipy==1.6.0'
-    ],
-
+    long_description_content_type="text/markdown",
+    url="https://github.com/min0ru/pong_rl",
+    author="min0ru",
+    author_email="2010min0ru@gmail.com",
+    packages=find_packages(where="pong_rl"),
+    python_requires=">=3.6, <4",
+    install_requires=["gym[atari]", "tensorflow==2.4.1", "numpy==1.19.2", "scipy==1.6.0"],
     extras_require={
-        'dev': [
-            'ipython',
-            'jedi<0.18',
-            'jupyter',
-            'jupyterlab',
+        "dev": [
+            "ipython",
+            "jedi<0.18",
+            "jupyter",
+            "jupyterlab",
+            "pytest",
+            "pytest-coverage",
+            "isort",
+            "black",
         ],
     },
 )
