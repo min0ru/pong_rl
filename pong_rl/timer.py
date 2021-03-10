e8220fe3de62ef0f5650b2816a1b6c26ab878387
min0ru <2010min0ru@gmail.com>
2021-03-10 21:04:40 +0300

    ContextTimer now supports optional external logger.

--- pong_rl/timer.py
+++ pong_rl/timer.py
@@ -2,8 +2,9 @@ import time
 
 
 class ContextTimer:
-    def __init__(self, name):
+    def __init__(self, name, logger=None):
         self.name = name
+        self.logger = logger
 
     def __enter__(self):
         self.start_time = time.time()
@@ -11,4 +12,8 @@ class ContextTimer:
     def __exit__(self, *args, **kwargs):
         self.end_time = time.time()
         self.total_time = self.end_time - self.start_time
-        print(f"[{self.name}] seconds: {self.total_time}")
+        message = f"[{self.name}] seconds: {self.total_time}"
+        if self.logger:
+            self.logger.info(message)
+        else:
+            print(message)
