1efd1a0e9ea1d6a6e9adc55e99b41959ae379de8
min0ru <2010min0ru@gmail.com>
2021-03-15 17:39:12 +0300

    Redesigned agent classes so it's now more abstract from environments.

--- pong_rl/agents.py
+++ pong_rl/agents.py
@@ -4,17 +4,14 @@ import numpy as np
 from scipy.special import softmax
 from tensorflow import keras
 
-from .actions import PongAction
 
+class BaseAgent(abc.ABC):
+    """ Base Abstract class for trainable agent. """
 
-class BasePongAgent(abc.ABC):
-    """ Base Abstract class for trainable Pong Agent. """
-
-    INPUT_SHAPE = (80, 80)  # Processed input shape
-    ACTIONS_LEN = len(PongAction)
-
-    def __init__(self):
+    def __init__(self, actions_len, observation_shape):
         """ Agent initialization. """
+        self.actions_len = actions_len
+        self.observation_shape = observation_shape
         self.trainings = 0
         self.trained_observations = 0
 
@@ -40,48 +37,30 @@ class BasePongAgent(abc.ABC):
         pass
 
 
-class PongAgentRandom(BasePongAgent):
-    """ Pong Agent that implements random acton choice. """
-
-    def _predict_impl(self, observations):
-        """ Predictions is made randomly. """
-        observations_num = len(observations)
-        return softmax(np.random.randn(observations_num, self.ACTIONS_LEN), axis=1)
-
-    def _train_impl(self, observations, actions, rewards, **kwargs):
-        """ Random Agent is unable to train. """
-        return None
-
-    @property
-    def summary(self):
-        """ Agent description. """
-        return "RandomAgent"
-
-
-class BasePongAgentTF(BasePongAgent):
-    """ Abstract Base Class for TensorFlow agents. """
+class BaseAgentML(BaseAgent):
+    """ Abstract Base Class for ML model-based agents. """
 
     def __init__(self, *args, **kwargs):
-        """ TF agent initialization is model creation. """
-        super().__init__()
-        self._model = self._create_model(*args, **kwargs)
+        """ Agent initialization is model creation. """
+        super().__init__(*args, **kwargs)
+        self._model = self._create_model(**kwargs)
 
-    def _create_model(self, *args, **kwargs):
+    def _create_model(self, **kwargs):
         """ Model creation interface method. """
-        self._model = self._create_model_impl(self, *args, **kwargs)
+        self._model = self._create_model_impl(**kwargs)
         return self._model
 
     @abc.abstractmethod
-    def _create_model_impl(self, *args, **kwargs):
-        """ Method should initialize TF model and return it. """
+    def _create_model_impl(self, **kwargs):
+        """ Method should initialize model and return it. """
         pass
 
     def _predict_impl(self, observations):
-        """ Agent prediction using TensorFlow NN model. """
+        """ Agent prediction using ML model. """
         return self._model(self._prepare_observations(observations)).numpy()
 
     def _train_impl(self, observations, actions, rewards, **kwargs):
-        """ Training NN model with given observations. """
+        """ Training ML model with given observations. """
         return self._model.fit(
             self._prepare_observations(observations), actions, sample_weight=rewards, **kwargs
         )
@@ -91,14 +70,32 @@ class BasePongAgentTF(BasePongAgent):
         return observations
 
 
-class PongAgentTFConv(BasePongAgentTF):
-    """ Pong Agent based on TensorFlow NeuralNetworks. """
+class AgentRandom(BaseAgent):
+    """ Agent that implements random acton choice. """
+
+    def _predict_impl(self, observations):
+        """ Predictions is made randomly. """
+        observations_num = len(observations)
+        return softmax(np.random.randn(observations_num, self.actions_len), axis=1)
+
+    def _train_impl(self, observations, actions, rewards, **kwargs):
+        """ Random Agent is unable to train. """
+        return None
+
+    @property
+    def summary(self):
+        """ Agent description. """
+        return "AgentRandom"
+
+
+class AgentKerasConv(BaseAgentML):
+    """ Agent based on Keras (TensorFlow) Convolutional Neural Network. """
 
     def _create_model_impl(self, learning_rate=1e-3):
         """ Creating and compiling TF (Keras) model. """
         model = keras.Sequential(
             [
-                keras.Input(shape=(self.INPUT_SHAPE + (1,))),
+                keras.Input(shape=(self.observation_shape + (1,))),
                 keras.layers.Conv2D(8, 8),
                 keras.layers.MaxPool2D(4),
                 keras.layers.Conv2D(16, 4),
@@ -109,11 +106,11 @@ class PongAgentTFConv(BasePongAgentTF):
                 keras.layers.Dense(128, activation="relu"),
                 keras.layers.Dense(256, activation="relu"),
                 keras.layers.Dense(32, activation="relu"),
-                keras.layers.Dense(self.ACTIONS_LEN, activation="softmax"),
+                keras.layers.Dense(self.actions_len, activation="softmax"),
             ]
         )
         model.compile(
-            optimizer=keras.optimizers.Adam(learning_rate=1e-3),
+            optimizer=keras.optimizers.Adam(learning_rate),
             loss=keras.losses.CategoricalCrossentropy(),
             metrics=[
                 keras.metrics.Accuracy(),
